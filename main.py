090c1d986ac8ed7c44056811d2ce3adcbe677a29
kauserahmed <54607183+kauserahmed@users.noreply.github.com>
2021-05-05 04:00:42 -0400

    Initilizing brand name generator file.

---
+++ main.py
@@ -0,0 +1,8 @@
+# Create a greeting for your program.
+print("Welcome to the brand name generator!!!")
+#2. Ask the user for the city that they grew up in.
+city = input("Which city you grow up?\n")
+#3. Ask the user for the name of a pet.
+pet_name = input("What is your pet name?\n")
+
+print("Your brand name could be " + (city + " " + pet_name))
